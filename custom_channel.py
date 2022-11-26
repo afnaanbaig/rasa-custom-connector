d492703cf21d765206b502b311ea2c8ade782be6
rehan-issmai <rehan@issm.ai>
2022-11-27 03:01:05 +0500

    custom connectors for rasa

--- custom_channel.py
+++ custom_channel.py
@@ -1,58 +1,47 @@
-import logging
-import uuid
+import asyncio
 import inspect
-import rasa
-import json
-from sanic import Blueprint, response
+from sanic import Sanic, Blueprint, response
 from sanic.request import Request
-from socketio import AsyncServer
-from typing import Text, List, Dict, Any, Optional, Callable, Iterable, Awaitable
+from sanic.response import HTTPResponse
+from typing import Text, Dict, Any, Optional, Callable, Awaitable, NoReturn
 from asyncio import Queue, CancelledError
-from rasa.core.channels.channel import UserMessage, OutputChannel, CollectingOutputChannel, InputChannel
+import rasa.utils.endpoints
+from rasa.core.channels.channel import (
+    InputChannel,
+    CollectingOutputChannel,
+    UserMessage,
+)
 
-logger = logging.getLogger(__name__)
-
-class MyIo(InputChannel):
-    """A custom http input channel.
-
-    This implementation is the basis for a custom implementation of a chat
-    frontend. You can customize this to send messages to Rasa Core and
-    retrieve responses from the agent."""
-
+class MyioInput(InputChannel):
     @classmethod
-    def name(cls):
-        print("hi from name method")
+    def name(cls) -> Text:
         return "myio"
+
 
     @staticmethod
     async def on_message_wrapper(
-        on_new_message: Callable[[UserMessage], Awaitable[None]],
+        on_new_message: Callable[[UserMessage], Awaitable[Any]],
         text: Text,
         queue: Queue,
         sender_id: Text,
+        input_channel: Text,
+        metadata: Optional[Dict[Text, Any]],
     ) -> None:
-
-        print("Inside on_message_wrapper function")
         collector = QueueOutputChannel(queue)
 
         message = UserMessage(
-            text, collector, sender_id, input_channel=RestInput.name()
+            text, collector, sender_id, input_channel=input_channel, metadata=metadata
         )
-
-        print("above on_new_message method")
         await on_new_message(message)
 
-        await queue.put("DONE")  # pytype: disable=bad-return-type
+        await queue.put("DONE")
 
-    async def _extract_sender(self, req) -> Optional[Text]:
+    async def _extract_sender(self, req: Request) -> Optional[Text]:
         return req.json.get("sender", None)
 
-    def _extract_metadata(self, req: Request) -> Text:
-        return req.json.get("metadata") or self.name()
-
     # noinspection PyMethodMayBeStatic
-    def _extract_message(self, req):
-        print("User message ::- ",req.json.get("message", None))
+
+    def _extract_message(self, req: Request) -> Optional[Text]:
         return req.json.get("message", None)
 
     def _extract_input_channel(self, req: Request) -> Text:
@@ -63,23 +52,31 @@ class MyIo(InputChannel):
         on_new_message: Callable[[UserMessage], Awaitable[None]],
         text: Text,
         sender_id: Text,
+        input_channel: Text,
+        metadata: Optional[Dict[Text, Any]],
     ) -> Callable[[Any], Awaitable[None]]:
         async def stream(resp: Any) -> None:
             q = Queue()
             task = asyncio.ensure_future(
-                self.on_message_wrapper(on_new_message, text, q, sender_id)
+                self.on_message_wrapper(
+                    on_new_message, text, q, sender_id, input_channel, metadata
+                )
             )
             while True:
-                result = await q.get()  # pytype: disable=bad-return-type
+                result = await q.get()
                 if result == "DONE":
                     break
                 else:
                     await resp.write(json.dumps(result) + "\n")
             await task
 
-        return stream  # pytype: disable=bad-return-type
+        return stream
 
-    def blueprint(self, on_new_message: Callable[[UserMessage], Awaitable[None]]):
+
+
+    def blueprint(
+        self, on_new_message: Callable[[UserMessage], Awaitable[None]]
+    ) -> Blueprint:
         custom_webhook = Blueprint(
             "custom_webhook_{}".format(type(self).__name__),
             inspect.getmodule(self).__name__,
@@ -87,55 +84,54 @@ class MyIo(InputChannel):
 
         # noinspection PyUnusedLocal
         @custom_webhook.route("/", methods=["GET"])
-        async def health(request: Request):
-            print("Inside health")
+        async def health(request: Request) -> HTTPResponse:
             return response.json({"status": "ok"})
 
         @custom_webhook.route("/webhook", methods=["POST"])
-        async def receive(request: Request):
-            print("Inside receive")
+        async def receive(request: Request) -> HTTPResponse:
             sender_id = await self._extract_sender(request)
             text = self._extract_message(request)
-            print("sender_id is ::-",sender_id)
-            print("text is ::-",text)
             should_use_stream = rasa.utils.endpoints.bool_arg(
                 request, "stream", default=False
             )
-
             input_channel = self._extract_input_channel(request)
+            metadata = self.get_metadata(request)
 
-            metadata = self._extract_metadata(request)
-            metadata = "{\"metadata\": \"" + str(metadata) + "\"}"
-            metadata = json.loads(metadata)
-            print('-metadata:', metadata)
             if should_use_stream:
                 return response.stream(
-                    self.stream_response(on_new_message, text, sender_id),
-                     content_type="text/event-stream",
-                    
+                    self.stream_response(
+                        on_new_message, text, sender_id, input_channel, metadata
+                    ),
+                    content_type="text/event-stream",
                 )
             else:
-                collector = CollectingOutputChannel()
-                on_new_message(UserMessage(text, collector, sender_id))
-                print("collector MSG::",collector)
+                collector = MyioOutput()
                 # noinspection PyBroadException
                 try:
                     await on_new_message(
                         UserMessage(
-                            text, collector, sender_id, input_channel=input_channel, metadata=metadata
+                            text,
+                            collector,
+                            sender_id,
+                            input_channel=input_channel,
+                            metadata=metadata,
                         )
                     )
                 except CancelledError:
                     logger.error(
-                        "Message handling timed out for "
-                        "user message '{}'.".format(text)
+                        f"Message handling timed out for " f"user message '{text}'."
                     )
                 except Exception:
                     logger.exception(
-                        "An exception occured while handling "
-                        "user message '{}'.".format(text)
+                        f"An exception occured while handling "
+                        f"user message '{text}'."
                     )
                 return response.json(collector.messages)
-                
 
         return custom_webhook
+
+
+class MyioOutput(CollectingOutputChannel):
+    @classmethod
+    def name(cls) -> Text:
+        return "myio"
